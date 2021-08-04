08ed6bbf146667a1be173ea449fd9f3b111d5dd9
Michel <michelsimao@gmail.com>
2021-08-04 17:03:26 -0300

    Ajustes de layout e inclusão da funcao newQuery

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import requests
+import os
 
 def main():
+    os.system('clear')
     print("####################")
     print("### Consulta CEP ###")
     print("####################")
     print()
 
+    def newQuery():
+        option = int(input("Nova consulta?\n1 = Sim\n2 = Sair\n"))
+
+        if option == 1:
+            main()
+        else:
+            print("Adeus")
+            exit()
+
     cepInput = input("Digite o CEP para consulta: ")
 
     if len(cepInput) != 8:
+        print()
+        print("-------------------------------")
         print("Quantidade de dígitos inválida!")
-        exit()
+        print("-------------------------------")
+        print()
+        newQuery()
 
     request = requests.get("https://viacep.com.br/ws/{}/json/".format(cepInput))
 
@@ -18,6 +33,7 @@ def main():
 
     if "erro" not in addressData:
         print()
+        print("--------------------------------")
         print("==> CEP ENCONTRADO <==")
         print("CEP: {}".format(addressData['cep']))
         print("Logradouro: {}".format(addressData['logradouro']))
@@ -25,17 +41,16 @@ def main():
         print("Bairro: {}".format(addressData['bairro']))
         print("Cidade: {}".format(addressData['localidade']))
         print("Estado: {}".format(addressData['uf']))
+        print("--------------------------------")
+        print()
     else:
+        print()
+        print("---------------------")
         print("{}: CEP INVÁLIDO!".format(cepInput))
+        print("---------------------")
+        print()
 
-    print("------------------------------")
-
-    option = int(input("Nova consulta?\n1 = Sim\n2 = Sair\n"))
-
-    if option == 1:
-        main()
-    else:
-        print("Adeus")
+    newQuery()
 
 if __name__ == "__main__":
     main()
